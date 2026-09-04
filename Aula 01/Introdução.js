8733e9c781e54c2f58019e2e5e0b51c1d45d7750
Kauan SIlva <150639807+Kauz777@users.noreply.github.com>
2026-09-03 21:35:20 -0300

    Add files via upload

---
+++ Aula 01/Introdução.js
@@ -0,0 +1,19 @@
+// Javascript é uma linguagem fracamente tipada
+
+var VariavelVar = "Kauan Silva"
+let VariavelLet = 'TUrma de ADS 2026'
+const VariavelConst = 21
+
+console.log("Welcome New World")
+console.log(VariavelVar)
+console.log(VariavelVar, VariavelLet)
+console.log(VariavelVar + VariavelLet)
+console.log(VariavelConst + VariavelConst)
+
+VariavelLet = 2
+console.log(VariavelLet)
+
+// VariavelConst = 6 
+// A regra é que a constante não pode
+// ser reatribuida
+console.log(VariavelConst)
