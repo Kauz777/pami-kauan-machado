d10802b1422fd75b06abb816693070194c7a57f6
Kauan SIlva <150639807+Kauz777@users.noreply.github.com>
2026-09-03 22:04:41 -0300

    Add files via upload

--- Aula 01/Introdução.js
+++ Aula 01/Introdução.js
@@ -1,19 +1,36 @@
-// Javascript é uma linguagem fracamente tipada
+// EStudei antes do professor começar a coda, puxei o code do github (peço perdão professor, mas já sabia um pouco da máteria).
 
-var VariavelVar = "Kauan Silva"
-let VariavelLet = 'TUrma de ADS 2026'
-const VariavelConst = 21
+var Variavel1 = "Kauan Silva"
+let Variavel2 = 'Mega Rayquaza Shiny 6 IVS Perfeitos'
+
+
+//variavel da minha idade
+const Variavel3 = 21
+
+
+//new world
 
 console.log("Welcome New World")
-console.log(VariavelVar)
-console.log(VariavelVar, VariavelLet)
-console.log(VariavelVar + VariavelLet)
-console.log(VariavelConst + VariavelConst)
+console.log(Variavel2)
+console.log(Variavel1, Variavel2)
+console.log(Variavel1 + Variavel2)
+console.log(Variavel3 + Variavel3)
 
 VariavelLet = 2
 console.log(VariavelLet)
 
-// VariavelConst = 6 
+// Variavel3
 // A regra é que a constante não pode
 // ser reatribuida
-console.log(VariavelConst)
+console.log(Variavel3)
+
+
+//Parte que acompanhei da aula 
+
+
+let Teste = "texto";
+console.log(typeof(teste))
+teste = 2;
+console.log(typeof(teste));
+Teste = {nome: "João" , idade: 38};
+console.log(typeof(teste))
